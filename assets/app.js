eb5e332aa71f89a4ad77311adc9f97e5ea6f1d37
Jessica Blankemeier <jessicablankemeier@gmail.com>
2020-06-24 18:05:31 -0700

    added bootstrap cards for styling

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,5 @@
-var m = moment();
+var todayDate = moment();
+$("#currentDay").text(todayDate.format('dddd, ' + 'MMMM Do'));
 
 
 var citiesArray = JSON.parse(localStorage.getItem("cities")) || [];
@@ -9,35 +10,51 @@ $(function () {
     $("#search-button").on("click", handleSearch);
 });
 
-    function handleSearch(event) {
-        event.preventDefault();
-        var city = $("#city-input").val();
+function handleSearch(event) {
+    event.preventDefault();
+    var city = $("#city-input").val();
 
-        var APIKey = "085c19b21e169a594fa99752c60ac8c8";
+    var APIKey = "085c19b21e169a594fa99752c60ac8c8";
 
-        // URL to query the weather API database
-        var queryURL =
-            "https://api.openweathermap.org/data/2.5/forecast?q=" +
-            city +
-            "&appid=" +
-            APIKey +
-            "&units=imperial";
+    // URL to query the weather API database
+    var queryURL =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        city +
+        "&appid=" +
+        APIKey +
+        "&units=imperial";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function (response) {
-            console.log(response);
-            var forecastDays = [];
-            forecastDays.push(response.list[0]);
-            forecastDays.push(response.list[8]);
-            forecastDays.push(response.list[16]);
-            forecastDays.push(response.list[24]);
-            forecastDays.push(response.list[32]);
+    //calling 5 days using api that received data every 3 hours
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function (response) {
+        console.log(response);
+        var forecastDays = [];
+        forecastDays.push(response.list[0]);
+        forecastDays.push(response.list[8]);
+        forecastDays.push(response.list[16]);
+        forecastDays.push(response.list[24]);
+        forecastDays.push(response.list[32]);
 
-            $("#forecast").html("<pre>" + JSON.stringify(forecastDays, null, 2) + "</pre>");
-        });
-    }
+        //dump data
+        $("#forecast").html("<pre>" + JSON.stringify(forecastDays, null, 2) + "</pre>");
+    
+
+    //create forecast div
+    var forecastContainer = $("<div>").addClass("forecast-day");
+
+    //create elements for required date, icon, temp, and humidity
+
+    var dateEl = $("<div>").text("6/24/2020");
+    var iconEl = $("<div>").text("icon: 012");
+    var humidityEl = $("<div>").text("humidity");
+
+    forecastContainer.append(dateEl, iconEl, tempEl, humidityEl);
+    $("#forecast").append(forecastContainer);
+    });
+
+}
       //for (var i = 0; i < forecastDays.length; i += 1) {
       //  var forecastContainer = $("<div>").addClass("forecast-day");
       //  var dateEl = $("<div>").text(forecastDays[i].dt);
